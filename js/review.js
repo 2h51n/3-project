fdd5ee98463d4b77ec492b929612d37785703b91
kimsonghee430 <bokchi8@gmail.com>
2024-10-16 12:40:22 +0900

    소재별 카테고리 및 굿즈영역 완료

--- js/review.js
+++ js/review.js
@@ -1,24 +1,24 @@
-var swiper = new Swiper(".swiper-container", {
-  slidesPerView: 3, // 한 번에 3개의 슬라이드 표시
-  spaceBetween: 30, // 슬라이드 사이의 간격
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true, // 페이지네이션 클릭 가능
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true, // 스크롤바를 드래그로 조작 가능
-  },
-  breakpoints: {
-    // 화면 크기에 따른 반응형 슬라이드 수
-    1024: {
-      slidesPerView: 3,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    480: {
-      slidesPerView: 1,
-    },
-  },
-});
+// var swiper = new Swiper(".swiper-container", {
+//   slidesPerView: 3, // 한 번에 3개의 슬라이드 표시
+//   spaceBetween: 30, // 슬라이드 사이의 간격
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true, // 페이지네이션 클릭 가능
+//   },
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//     draggable: true, // 스크롤바를 드래그로 조작 가능
+//   },
+//   breakpoints: {
+//     // 화면 크기에 따른 반응형 슬라이드 수
+//     1024: {
+//       slidesPerView: 3,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     480: {
+//       slidesPerView: 1,
+//     },
+//   },
+// });

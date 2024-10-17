1eac037e73c0c3c6e19362a5211a9f1efe001e11
2h51n <lhi7247@gmail.com>
2024-10-17 17:26:40 +0900

    이벤트,메테리얼 , 인기상품, 리뷰, 구독 반응형 수정, 메테리얼 js 기능 추가 , 이벤트 배너 360 이미지 추가

--- js/material.js
+++ js/material.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // 각 카테고리 버튼 및 관련 상품 영역
   const categoryButtons = document.querySelectorAll(".swMaterial .swiper-slide a");
   const materialGoodsSections = document.querySelectorAll(".material-goods");
+  const showMoreButton = document.getElementById("showMoreButton"); // 더 보기 버튼
 
   // 모든 카테고리 상품 숨기기 함수
   function hideAllCategories() {
@@ -43,7 +44,6 @@ document.addEventListener("DOMContentLoaded", function () {
       adjustGoodsVisibility(); // 카테고리가 변경될 때마다 굿즈 가시성 재조정
     });
   });
-
   // 페이지 로드 시 기본으로 무작위 카테고리 및 해당 아이콘 표시
   hideAllCategories();
   deactivateAllIcons(); // 모든 아이콘 비활성화
@@ -51,13 +51,34 @@ document.addEventListener("DOMContentLoaded", function () {
   const randomCategory = materialGoodsSections[randomIndex]; // 무작위 카테고리 선택
   if (randomCategory) {
     randomCategory.style.display = "flex"; // 무작위 카테고리 표시
-
     // 선택된 카테고리에 해당하는 아이콘 활성화
     const categoryAlt = randomCategory.classList[1]; // 두 번째 클래스(예: 'plastic') 가져오기
     const relatedButton = Array.from(categoryButtons).find((button) => button.querySelector("img").alt.toLowerCase() === categoryAlt);
-
     if (relatedButton) {
       activateIcon(relatedButton); // 무작위 카테고리와 관련된 아이콘 활성화
     }
   }
+  // 더 보기 버튼 클릭 시 히든 클래스 토글
+  showMoreButton.addEventListener("click", function () {
+    if (window.innerWidth <= 480) {
+      const hiddenItems = document.querySelectorAll(".hidden"); // .hidden 클래스를 가진 모든 요소 선택
+      hiddenItems.forEach(function (item) {
+        // 현재 요소의 display 속성에 따라 보이게 하거나 숨기기
+        if (item.style.display === "none" || item.style.display === "") {
+          item.style.display = "block"; // 보이게 설정
+        } else {
+          item.style.display = "none"; // 숨기기 설정
+        }
+      });
+    }
+  });
+  // 윈도우 크기 변경 시 .hidden 클래스 활성화
+  // window.addEventListener("resize", function () {
+  //   if (window.innerWidth > 481) {
+  //     const hiddenItems = document.querySelectorAll(".hidden");
+  //     hiddenItems.forEach(function (item) {
+  //       item.style.display = "block"; // 숨기기
+  //     });
+  //   }
+  // });
 });

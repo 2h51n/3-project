4492e1ee52f19a06f8adf14265a3672dbb5eca47
kimsonghee430 <bokchi8@gmail.com>
2024-10-17 09:44:10 +0900

    헤더 서브네비 및 기능 추가

--- js/header.js
+++ js/header.js
@@ -3,7 +3,7 @@ window.onload = function () {
   const swBanner = new Swiper(".swBanner", {
     loop: true, // loop : 무한으로 도는 것.
     autoplay: {
-      delay: 4000,
+      delay: 3500,
       disableOnInteraction: false, // 상관없이 계속 autoplay.
     },
   });
@@ -19,6 +19,48 @@ window.onload = function () {
     // },
 
 });
-// ===메인 배너 슬라이드 영역 끝
+
+  const swReview = new Swiper(".swReview", {
+    slidesPerView: 1, 
+    spaceBetween: 20,
+    loop: true,
+
+
+    breakpoints: {
+      // 화면 너비가 320px 이상일 때
+      320: {
+        slidesPerView: 1.5,
+      },
+      // 화면 너비가 480px 이상일 때
+      480: {
+        slidesPerView: 2,
+      },
+      // 화면 너비가 768px 이상일 때
+      500: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      // 화면 너비가 1024px 이상일 때
+      950: {
+        slidesPerView: 3.5,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+
+
+
+});
+// ===슬라이드 영역 끝
+
+// 헤더 카테고리 클릭 시 서브메뉴 나옴
+const cateIcon = document.querySelector("#category-icon")
+const naviCate = document.querySelector(".navi-cate")
+cateIcon.addEventListener("click", function(){
+  naviCate.classList.toggle("active")
+})
 
 }
